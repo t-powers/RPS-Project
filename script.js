728ec6e8d4e9e6b94e44409c8cbd87f7dc05eb9a
t-powers <127955989+t-powers@users.noreply.github.com>
2023-09-03 11:04:51 -0400

    created random integer
    integer returns 1 - 3 inclusively

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ pseudocode plan
 get computer's choice
 add function called getComputerChoice
 randomly pick number and switch number to rock, paper, scissors
-
-
-
 */
+
+function getComputerChoice() {
+  const randNum = Math.floor(Math.random() * 3) + 1;
+  return randNum;
+}
+console.log(getComputerChoice());
