1459145a28a7cf05854df82f1e2f1fafde35df2d
t-powers <127955989+t-powers@users.noreply.github.com>
2023-09-03 14:36:24 -0400

    finished switch and added comments

--- script.js
+++ script.js
@@ -7,15 +7,22 @@ add function called getComputerChoice
 randomly pick number and switch number to rock, paper, scissors
 */
 
+//create random integer 1 || 2 || 3
 function createRandNum() {
   const randNum = Math.floor(Math.random() * 3) + 1;
   return randNum;
 }
-
-switch (createRandNum()){
+//switch createRandNum function int to RPS word
+switch (createRandNum()) {
   case 1:
     console.log("Rock");
-  break;
+    break;
+  case 2:
+    console.log("Paper");
+    break;
+  case 3:
+    console.log("Scissors");
+    break;
   default:
     console.log("Try again.");
 }
