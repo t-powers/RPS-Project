e0ad42e53e5d6adb8417a99a1c09bfc128856a4b
t-powers <127955989+t-powers@users.noreply.github.com>
2023-09-03 22:01:56 -0400

    removed info

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-//planning
-
-/*
-pseudocode plan
-get computer's choice
-add function called getComputerChoice
-randomly pick number and switch number to rock, paper, scissors
-*/
-
 //create random integer 1 || 2 || 3
 function getComputerChoice() {
   const randNum = Math.floor(Math.random() * 3) + 1;
