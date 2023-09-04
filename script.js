11940a47288938fca8ca078acda4e9a2e4f90957
t-powers <127955989+t-powers@users.noreply.github.com>
2023-09-04 10:17:01 -0400

    combined number generator with switch statement

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 //create random integer 1 || 2 || 3
-const getComputerChoice = Math.floor(Math.random() * 3) + 1;
-
 //switch createRandNum function int to RPS word
-
-switch (getComputerChoice()) {
-  case 1:
-    console.log("Rock");
-    break;
-  case 2:
-    console.log("Paper");
-    break;
-  case 3:
-    console.log("Scissors");
-    break;
-  default:
-    "Try again.";
+function getComputerChoice() {
+  const randNum = Math.floor(Math.random() * 3) + 1;
+  switch (randNum) {
+    case 1:
+      console.log("Rock");
+      break;
+    case 2:
+      console.log("Paper");
+      break;
+    case 3:
+      console.log("Scissors");
+      break;
+    default:
+      "Try again.";
+  }
 }
-//play a round of RPS
+getComputerChoice();
