5abaf1814eefb74e3d8ba226673426fe91edd994
t-powers <127955989+t-powers@users.noreply.github.com>
2023-09-09 15:40:41 -0400

    Revert "added comments"
    This reverts commit 654749859483515e317ce2ce793c81af0035cf0f.

--- script.js
+++ script.js
@@ -35,11 +35,9 @@ function playGame(userPick, compPick) {
   return choice;
 }
 
-function playerChoice() {
-  const prompt = require("prompt-sync")(); //prompt requirement
-
-  const playerPrompt = prompt("Choose rock, paper, scissors. "); //assign player's choice
 const prompt = require("prompt-sync")();
+
+const playerPrompt = prompt("Choose rock, paper, scissors. ");
 
 const playerPrompt = prompt("Choose rock, paper, scissors. ");
 
@@ -48,5 +46,5 @@ const playerPrompt = prompt("Choose rock, paper, scissors. ");
   const userPick =
     toLowerString.charAt(0).toUpperCase() + toLowerString.slice(1);
 }
-// const compPick = getComputerChoice();
-// console.log(playGame(userPick, compPick));
+const compPick = getComputerChoice();
+console.log(playGame(userPick, compPick));
